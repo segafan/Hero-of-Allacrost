301bbab20741332c3fcd077e48a8690b5d648f23
bmbalthazar <bmbalthazar@localhost>
2005-05-19 02:15:36 +0000

    The actual battle code files.

---
+++ module_00/src/battle.cpp
@@ -0,0 +1,115 @@
+/* 
+ * battle.cpp
+ *	Code for Hero of Allacrost battle mode
+ *	(C) 2005 by Tim Hargreaves
+ *
+ *	This code is licensed under the GNU GPL. It is free software and you may modify it 
+ *	 and/or redistribute it under the terms of this license. See http://www.gnu.org/copyleft/gpl.html
+ *	 for details.
+ */
+ 
+/*
+ * The code in this file is for **** finish this ****
+ */
+
+#include <iostream>
+#include "battle.h"
+#include "audio.h"
+#include "video.h"
+#include "engine.h"
+#include "global.h" //<< not ready yet
+#include "data.h"
+
+using namespace std;
+using namespace hoa_battle::local_battle;
+using namespace hoa_utils;
+using namespace hoa_audio;
+using namespace hoa_video;
+using namespace hoa_engine;
+using namespace hoa_data;
+
+
+/*
+
+To Tim: I already gave you this list yesterday, but here are the most important files for you to
+look through and understand what functions/data is available to you:
+
+audio.h, video.h, data.h, engine.h, utils.h
+
+Within those files, you should look at the definitions of the following classes so you know what
+functions you can call and what they do:
+
+GameAudio, GameVideo, GameData, GameInput, GameMode, GameModeManager, GameSettings
+
+
+The best example of a "mode" class right now is MapMode in map.*. It's pretty big and kind of ugly,
+but just look at the MapMode class members and functions and ignore the other classes in there. (All
+the MapMode stuff is at the bottom of both files). Good luck!
+
+
+*/
+
+
+
+namespace hoa_battle {
+
+
+/* 
+
+To Tim: From the GameMode class that BattleMode inherits from, you already have working access to
+all of the game's singleton classes so you don't need to worry about allocating pointers to them, etc.
+You can find them all inside the GameMode class in engine.h, but here's a reference of them for your
+convenience:
+
+	hoa_audio::GameAudio *AudioManager;
+	hoa_video::GameVideo *VideoManager;
+	hoa_data::GameData *DataManager;
+	GameInput *InputManager;
+	GameModeManager *ModeManager;
+	GameSettings *SettingsManager;
+
+*/
+
+
+BattleMode::BattleMode() {
+	cerr << "DEBUG: BattleMode's constructor invoked." << endl;
+	
+	// Load the map from the Lua data file
+	//DataManager->LoadMap(this, map_id);
+	
+	// Setup the coordinate system
+	VideoManager->SetCoordSys(0, SCREEN_LENGTH, 0, SCREEN_HEIGHT, 1);
+	// To Tim: This sets up a cooriate system where "0, 0" is the top left hand corner of the screen,
+	// extends to SCREEN_LENGTH and SCREEN_HEIGHT, and has 1 depth level (ie, it's 2D)
+	m_iCounter = 0;
+}
+
+
+BattleMode::~BattleMode() {
+	// Clean up any allocated music/images/sounds/whatever data here. Don't forget! ^_~
+	cerr << "DEBUG: BattleMode destructor invoked." << endl;
+}
+
+
+void BattleMode::Update(Uint32 time_elapsed) {
+	// This function is the top level function that updates the status of the game. You'll likely write
+	// several sub-functions that this function calls to keep the size of the function a sane amount.
+	// For example, UpdateCharacters() and UpdateEnemies(). Make these sub-functions private, because
+	// nothing else should need to know about them.
+	
+	// time_elapsed tells us how long it's been since the last time this function was called.
+	++m_iCounter;
+	cerr << m_iCounter << endl;
+	
+	if (m_iCounter >= 1000)
+		ModeManager->Pop();
+}
+
+
+void BattleMode::Draw() {
+	// This function draws the next frame that will be displayed to the screen. Like Update(), you'll
+	// probably write several sub-functions to keep the size of this function manageable. 
+
+}
+
+} // namespace hoa_battle
---
+++ module_00/src/battle.h
@@ -0,0 +1,98 @@
+/* 
+ * battle.h
+ *	Header file for Hero of Allacrost battle mode
+ *	(C) 2005 by Tim Hargreaves
+ *
+ *	This code is licensed under the GNU GPL. It is free software and you may modify it 
+ *	 and/or redistribute it under the terms of this license. See http://www.gnu.org/copyleft/gpl.html
+ *	 for details.
+ */
+
+/*
+ * The code in this file is for ****finish this****
+ */
+ 
+ 
+/*
+
+To Tim: Here are your skeleton files. This sets up all the basic crap you'll need, now you just have to
+make the crap do something useful. :) I think your first task should be drawing static objects on the screen 
+(since we have an objective of include a battle screenshot in our next released screens).
+
+
+Unfortunately, even if we have the code we lack the art. :P So you might need to make some really crappy
+placeholder graphics for the time being. Also unfortunately a lot of the classes that BattleMode will be
+using (for representing characters and enemies) will be in global.*, which have not been very refined yet.
+I'm working on that file though, so hopefully we can work together on it and figure out what it needs.
+
+
+For the battle code initialization, what I was thinking of doing was in MapMode I'll figure out what 
+enemies to have in the battle and how many, and then I will pass you a vector of enemy objects in the 
+battle mode constructor or something. Once you've got that, you'll have to figure out how to place the
+enemies on the enemy grid area of the battle map. ^_~
+
+
+
+One final note. So that you can get to BattleMode to test it when you're actually running the game, I'm
+going to add a temporary shortcut in the BootMode code that will load BattleMode. I'll tell you what that
+is once I write it.
+
+*/
+ 
+#ifndef __BATTLE_HEADER__
+#define __BATTLE_HEADER__
+
+#include <string>
+#include <vector>
+#include "defs.h"
+#include "utils.h"
+#include "engine.h"
+
+namespace hoa_battle {
+
+
+/* The reason for this embedded namespace is so you can have constants with short and "friendly" names. Any
+constants you make available inside hoa_battle namespace, you must prefix with BATTLE_ according to our code
+standard (have you read it???) The only piece of code that uses the local_battle namespace should be battle.h
+and battle.cpp, nothing more. So name those constants whatever you want! =D
+*/
+namespace local_battle {
+
+const int TILE_SIZE = 64; // The virtual "tile map" that we discussed in the forums has square 64 pixel tiles
+const int SCREEN_LENGTH = 16; // Number of tiles long the screen is
+const int SCREEN_HEIGHT = 12; // The number of tiles high the screen is
+ 
+}
+ 
+ /******************************************************************************
+	BattleMode Class
+		
+	>>>members<<<
+
+	>>>functions<<<
+
+	>>>notes<<<
+ *****************************************************************************/
+class BattleMode : public hoa_engine::GameMode {
+private:
+	friend class hoa_data::GameData;
+	
+	std::vector<hoa_video::ImageDescriptor> battle_images;	
+	std::vector<hoa_audio::MusicDescriptor> battle_music;
+	std::vector<hoa_audio::SoundDescriptor> battle_sound;
+	
+	int num_enemies;
+	
+	
+public: 
+	BattleMode();
+	~BattleMode();
+	
+	void Update(Uint32 time_elapsed);
+	void Draw();
+};
+
+
+} // namespace hoa_battle
+
+#endif
