c053d42446f8abb227cdf2c6e2c61e8a563dd86a
visage <visage@localhost>
2005-11-26 19:33:55 +0000

    Initial commit.  Base classes coded. No comments.

--- module_00/src/battle.cpp
+++ module_00/src/battle.cpp
@@ -75,23 +75,809 @@ convenience:
 
 */
 
+/**
+ * Actor
+ *
+ *
+ *
+ *
+ *
+ */
 
-BattleMode::BattleMode() {
+Actor::Actor(BattleMode *bm, uint32 x, uint32 y) {
+	_ownerBattleMode = bm;
+	_X_Location = x;
+	_Y_Location = y;
+	_isMoveCapable = true;
+	_nextAction = NULL;
+	_mode = new IdleMode(this);
+}
+
+Actor::Actor(const Actor&  a) {
+	_X_Location = a._X_Location;
+	_Y_Location = a._Y_Location;
+	_mode = a._mode;
+	_effects = a._effects;
+	_maxSkillPoints = a._maxSkillPoints;
+	_currentSkillPoints = a._currentSkillPoints;
+	_isMoveCapable = a._isMoveCapable;
+	_isAlive = a._isAlive;
+	_nextAction = a._nextAction;
+	_warmingUp = a._warmingUp;
+	_defensiveMode = a._defensiveMode;
+	_supporters = a._supporters;
+	
+	_mode = new IdleMode(this);
+}
+
+Actor::~Actor() {
+	//get rid of the next move, if we have one
+	delete _nextAction;
+	//delete our current mode
+	delete _mode;
+	
+	std::list<ActorEffect *>::iterator it = _effects.begin();
+	for(;it != _effects.end(); ) {
+		delete *it;
+	}
+}
+
+BattleMode *Actor::GetOwnerBattleMode() {
+	return _ownerBattleMode;
+}
+
+void Actor::SetMode(ActorMode *m) {
+	delete _mode; //delete on NULL does nothing
+	_mode = m;
+}
+
+ActorMode *Actor::GetMode() {
+	return _mode;
+}
+
+void Actor::UpdateEffects(uint32 dt) {
+	std::list<ActorEffect *>::iterator it = _effects.begin();
+	for(;it != _effects.end(); ) {
+		(*it)->Update(dt);
+		if((*it)->GetTTL() == 0) {
+			(*it)->UndoEffect();
+			_effects.erase(it);
+		}
+		else {
+			it++;
+		}
+	}
+}
+
+void Actor::PushEffect(ActorEffect *e) {
+	_effects.push_back(e);
+}
+
+void Actor::RemoveEffect(ActorEffect *e) {
+	_effects.remove(e);
+	delete e;
+}
+
+void Actor::PerformAction() {
+	_nextAction->PerformAction();
+}
+
+void Actor::SetNextAction(Action *a) {
+	delete _nextAction;
+	_nextAction = a;
+}
+
+bool Actor::HasNextAction() {
+	return _nextAction == NULL;
+}
+
+bool Actor::IsMoveCapable() {
+	return _isMoveCapable;
+}
+
+void Actor::SetMoveCapable(bool capable) {
+	_isMoveCapable = capable;
+}
+
+void Actor::AddSupporter(Actor *a) {
+	_supporters.push_back(a);
+}
+
+void Actor::RemoveSupporter(Actor *a) {
+	_supporters.remove(a);
+}
+
+void Actor::SetDefensiveMode(bool d) {
+	_defensiveMode = d;
+}
+
+bool Actor::IsInDefensiveMode() {
+	return _defensiveMode;
+}
+
+bool Actor::IsWarmingUp() {
+	return _warmingUp;
+}
+
+void Actor::SetWarmingUp(bool warmup) {
+	_warmingUp = warmup;
+}
+
+void Actor::SetAnimation(std::string animationMode) {
+	//set the animation for this actor
+	std::cout << this << " is going into animation mode: " << animationMode << std::endl;
+}
+
+/**
+ * BattleUI
+ *
+ *
+ *
+ *
+ *
+ */
+
+BattleUI::BattleUI(BattleMode *bm) {
+	_bm = bm;
+}
+		
+Actor* BattleUI::GetSelectedActor() {
+	return _currentlySelectedActor;
+}
+
+void BattleUI::SetActorSelected(Actor *a) {
+	_currentlySelectedActor = a;
+}
+
+void BattleUI::DeselectActor() {
+	_currentlySelectedActor = NULL;
+}
+
+std::list<Actor *> BattleUI::GetSelectedArgumentActors() {
+	return _currentlySelectedArgumentActors;
+}
+
+void BattleUI::setActorAsArgument(Actor *a) {
+	_currentlySelectedArgumentActors.push_back(a);
+}
+
+void BattleUI::RemoveActorAsArgument(Actor *a) {
+	//we don't have to erase the actor because it will be
+	//cleaned up at the end of the battle mode when all
+	//actors are cleaned up
+	//everything should stay alive until then
+	_currentlySelectedArgumentActors.remove(a);
+}
+
+void BattleUI::SetNumberNecessarySelections(uint32 select) {
+	_necessarySelections = select;
+}
+
+/**
+ * PlayerActor
+ *
+ *
+ *
+ *
+ *
+ */
+
+PlayerActor::PlayerActor(GCharacter *wrapped, BattleMode *bm, int x, int y) 
+	: Actor(bm, x, y) {
+	_wrappedCharacter = wrapped;
+}
+
+PlayerActor::~PlayerActor() {
+	//don't delete _wrappedCharacter--it should exist outside
+	//of battle as well
+}
+
+void PlayerActor::Update(uint32 dt) {
+	std::cout << "Updating: " << this << std::endl;
+	//update the current effects on the player
+	std::cout << "\tUpdate effects." << std::endl;
+	UpdateEffects(dt);
+	//update the current mode on the player
+	std::cout << "\tUpdate mode." << std::endl;
+	GetMode()->Update(dt);
+
+}
+
+void PlayerActor::Draw() {
+	std::cout << "Draw: " << this << std::endl;
+	//draw outselves here
+}
+		
+std::vector<GSkill> PlayerActor::GetAttackSkills() {
+	return _wrappedCharacter->GetAttackSkills();
+}
+
+std::vector<GSkill> PlayerActor::GetDefenseSkills() {
+	return _wrappedCharacter->GetDefenseSkills();
+}
+
+std::vector<GSkill> PlayerActor::GetSupportSkills() {
+	return _wrappedCharacter->GetSupportSkills();
+}
+
+uint32 PlayerActor::GetAgility() {
+	return _wrappedCharacter->GetAgility();
+}
+
+uint32 PlayerActor::GetStrength() {
+	return _wrappedCharacter->GetStrength();
+}
+
+uint32 PlayerActor::GetMaxHealth() {
+	return _wrappedCharacter->GetMaxHP();
+}
+
+uint32 PlayerActor::GetSkillPoints() {
+	return _wrappedCharacter->GetSP();
+} 
+
+std::vector<GAttackPoint> PlayerActor::GetAttackPoints() {
+	return _wrappedCharacter->GetAttackPoints();
+} 
+
+uint32 PlayerActor::GetIntelligence() {
+	return _wrappedCharacter->GetIntelligence();
+} 
+
+uint32 PlayerActor::GetMaxSkillPoints() {
+	return _wrappedCharacter->GetMaxSP();
+} 
+
+std::string PlayerActor::GetName() {
+	return _wrappedCharacter->GetName();
+}
+
+uint32 PlayerActor::GetHealth() {
+	return _wrappedCharacter->GetSP();
+}
+
+
+/**
+ * EnemyActor
+ *
+ *
+ *
+ *
+ *
+ */
+
+EnemyActor::EnemyActor(GEnemy *ge, BattleMode *bm, int x, int y) 
+	: Actor(bm, x,  y) {
+	_wrappedEnemy = ge;
+}
+
+EnemyActor::~EnemyActor() {
+	delete _wrappedEnemy;
+}
+
+void EnemyActor::Update(uint32 dt) {
+	std::cout << "Updating: " << this << std::endl;
+	
+	std::cout << "\tUpdate effects." << std::endl;
+	UpdateEffects(dt);
+	
+	std::cout << "\tUpdate mode." << std::endl;
+	GetMode()->Update(dt);
+	
+	std::cout << "\tDo AI." << std::endl;
+	DoAI(dt);
+}
+
+void EnemyActor::Draw() {
+	std::cout << "Draw: " << this << std::endl;
+	//draw ourselves here
+}
+
+void EnemyActor::DoAI(uint32 dt) {
+	//do some artificial intelligence, or something like that.s
+}
+
+void EnemyActor::LevelUp(uint32 average_level) {
+	//[0.0, 1.0] RandomUnit()  
+	//uint32 GaussianValue(int32 mean, int32 range, bool positive_value)
+	uint32 level = hoa_utils::GaussianValue(average_level,
+													RandomNumber(-3, 3), true);
+	
+	uint32 base_health = _wrappedEnemy->GetBaseHitPoints();
+	uint32 base_exp = _wrappedEnemy->GetBaseExperiencePoints();
+	uint32 base_skill = _wrappedEnemy->GetBaseSkillPoints();
+	uint32 base_strength = _wrappedEnemy->GetBaseStrength();
+	uint32 base_intelligence = _wrappedEnemy->GetBaseIntelligence();
+	uint32 base_agility = _wrappedEnemy->GetBaseAgility();
+	
+	uint32 growth_health = _wrappedEnemy->GetGrowthHitPoints();
+	uint32 growth_exp = _wrappedEnemy->GetGrowthExperiencePoints();
+	uint32 growth_skill = _wrappedEnemy->GetGrowthSkillPoints();
+	uint32 growth_strength = _wrappedEnemy->GetGrowthStrength();
+	uint32 growth_intelligence = _wrappedEnemy->GetGrowthIntelligence();
+	uint32 growth_agility = _wrappedEnemy->GetGrowthAgility();
+	
+	for(uint32 i = 0; i < level; i++) {
+		base_health = base_health + hoa_utils::GaussianValue(growth_health,
+													RandomNumber(-3, 3), true);
+		base_exp = base_exp + hoa_utils::GaussianValue(growth_exp,
+													RandomNumber(-3, 3), true);
+		base_skill = base_skill+ hoa_utils::GaussianValue(growth_skill,
+													RandomNumber(-3, 3), true);
+		base_strength = base_strength + hoa_utils::GaussianValue(growth_strength,
+													RandomNumber(-3, 3), true);
+		base_intelligence = base_intelligence + hoa_utils::GaussianValue(
+													growth_intelligence,
+													RandomNumber(-3, 3), true);
+		base_agility = base_agility + hoa_utils::GaussianValue(growth_agility,
+													RandomNumber(-3, 3), true);
+	}
+
+	_wrappedEnemy->SetMaxHP(base_health);
+	_wrappedEnemy->SetXP(base_exp);
+	_wrappedEnemy->SetXPLevel(level);
+	_wrappedEnemy->SetSP(base_skill);
+	_wrappedEnemy->SetStrength(base_strength);
+	_wrappedEnemy->SetIntelligence(base_intelligence);
+	_wrappedEnemy->SetAgility(base_agility);
+}
+
+std::vector<GSkill> EnemyActor::GetSkills() {
+	return _wrappedEnemy->GetSkills();
+	//here we need to deal with the current level and the level required to use
+	//the skill
+}
+
+std::vector<GAttackPoint> EnemyActor::GetAttackPoints() {
+	return _wrappedEnemy->GetAttackPoints();
+}
+
+uint32 EnemyActor::GetAgility() {
+	return _wrappedEnemy->GetAgility();
+}
+
+uint32 EnemyActor::GetStrength() {
+	return _wrappedEnemy->GetStrength();
+}
+
+uint32 EnemyActor::GetIntelligence() {
+	return _wrappedEnemy->GetIntelligence();
+}
+
+uint32 EnemyActor::GetHealth() {
+	return _wrappedEnemy->GetHP();
+}
+
+uint32 EnemyActor::GetMaxHealth() {
+	return _wrappedEnemy->GetMaxHP();
+}
+
+uint32 EnemyActor::GetSkillPoints() {
+	return _wrappedEnemy->GetSP();
+}
+
+uint32 EnemyActor::GetMaxSkillPoints() {
+	return _wrappedEnemy->GetMaxSP();
+}
+
+std::string EnemyActor::GetName() {
+	return _wrappedEnemy->GetName();
+}
+
+/**
+ * ActorMode
+ *
+ *
+ *
+ *
+ *
+ */
+
+ActorMode::ActorMode(Actor *a) {
+	_host = a;
+}
+
+ActorMode::~ActorMode() {
+
+}
+
+Actor *ActorMode::GetHost() {
+	return _host;
+}
+
+/**
+ * SupportMode
+ *
+ *
+ *
+ *
+ *
+ */
+
+SupportMode::SupportMode(uint32 TTL, Actor *a, std::list<Actor *> supported) : 
+	ActorMode(a) {
+	_supported = supported;
+	std::list<Actor *>::iterator it = _supported.begin();
+	for(; it != _supported.end(); it++) {
+		(*it)->AddSupporter(a);
+	}
+	
+	a->SetAnimation("SUPPORT");
+}
+
+void SupportMode::Update(uint32 dt) {
+	//SubtractTTL(dt);
+}
+
+void SupportMode::UndoMode() {
+	Actor *host = GetHost();
+	std::list<Actor *>::iterator it = _supported.begin();
+	for(; it != _supported.end(); it++) {
+		(*it)->RemoveSupporter(host);
+	}
+	host->SetMode(new IdleMode(host));
+}
+
+/**
+ * DefensiveMode
+ *
+ *
+ *
+ *
+ *
+ */
+
+DefensiveMode::DefensiveMode(Actor *a) : 
+	ActorMode(a) {
+	a->SetDefensiveMode(true);
+	a->SetAnimation("DEFENSIVE");
+}
+
+void DefensiveMode::Update(uint32 dt) {
+	//falala, nothing to do
+}
+
+void DefensiveMode::UndoMode() {
+	Actor *host = GetHost();
+	host->SetDefensiveMode(false);
+	host->SetMode(new IdleMode(host));
+}
+
+/**
+ * IdleMode
+ *
+ *
+ *
+ *
+ *
+ */
+
+IdleMode::IdleMode(Actor *a) : 
+	ActorMode(a) {
+	a->SetAnimation("IDLE");
+}
+
+void IdleMode::Update(uint32 dt) {
+	//falala, nothing to do
+}
+
+void IdleMode::UndoMode() {
+}
+
+/**
+ * WarmUpMode
+ *
+ *
+ *
+ *
+ *
+ */
+
+WarmUpMode::WarmUpMode(uint32 TTL, Actor *a) : 
+	ActorMode(a) {
+	_TTL = TTL;
+	a->SetWarmingUp(true);
+	a->SetAnimation("WARMUP");
+}
+
+void WarmUpMode::Update(uint32 dt) {
+	_TTL = _TTL - dt;
+	if(_TTL < 0) _TTL = 0;
+	
+	if(_TTL == 0) {
+		UndoMode();
+	}
+}
+
+void WarmUpMode::UndoMode() {
+	//tell the actor we are ready to perform the action
+	Actor *host = GetHost();
+	host->SetWarmingUp(false);
+}
+
+/**
+ * CoolDownMode
+ *
+ *
+ *
+ *
+ *
+ */
+
+CoolDownMode::CoolDownMode(uint32 TTL, Actor *a) : 
+	ActorMode(a) {
+	_TTL = TTL;
+	a->SetWarmingUp(true);
+	a->SetAnimation("COOLDOWN");
+}
+
+void CoolDownMode::Update(uint32 dt) {
+	_TTL = _TTL - dt;
+	if(_TTL < 0) _TTL = 0;
+	
+	if(_TTL == 0) {
+		UndoMode();
+	}
+}
+
+void CoolDownMode::UndoMode() {
+	//tell the actor we are ready to perform the action
+	Actor *host = GetHost();
+	host->SetMode(new IdleMode(host));
+}
+
+/**
+ * VisualEffect
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+ 
+VisualEffect::VisualEffect(std::string am, hoa_video::AnimatedImage i) {
+	_animationMode = am;
+	_image = i;
+}
+
+void VisualEffect::Draw() {
+
+}
+
+std::string VisualEffect::GetAnimationMode() {
+	return _animationMode;
+}
+
+/**
+ * ActorEffect
+ *
+ *
+ *
+ *
+ *
+ */
+
+ActorEffect::ActorEffect(Actor *a, std::string name, uint32 ttl, uint32 ctc,
+					uint32 ul, VisualEffect *ve, SoundDescriptor se) {
+	_host = a;
+	_EffectName = name;
+	_TTL = ttl;
+	_chanceToCure = ctc;
+	_updateLength = ul;
+	_visualEffect = ve;
+	_soundEffect = se;
+	
+	//here, we actually need system time
+	_age = 0;
+	_lastUpdate = _age;
+}
+
+ActorEffect::~ActorEffect() {
+	delete _visualEffect;
+}
+
+uint32 ActorEffect::GetTTL() {
+	return _TTL;
+}
+
+void ActorEffect::SubtractTTL(uint32 dt) {
+	_TTL = _TTL - dt;
+	if(_TTL < 0) _TTL = 0;
+}
+
+void ActorEffect::Update(uint32 dt) {
+	SubtractTTL(dt);
+}
+
+Actor *ActorEffect::GetHost() {
+	return _host;
+}
+
+std::string ActorEffect::GetEffectName() {
+	return _EffectName;
+}
+
+uint32 ActorEffect::GetChanceToCure() {
+	return _chanceToCure;
+}
+
+uint32 ActorEffect::GetUpdateLength() {
+	return _updateLength;
+}
+
+uint32 ActorEffect::GetLastUpdate() {
+	return _lastUpdate;
+}
+
+void ActorEffect::SetLastUpdate(uint32 lu) {
+	_lastUpdate = lu;
+}
+
+VisualEffect *ActorEffect::GetVisualEffect() {
+	return _visualEffect;
+}
+
+SoundDescriptor ActorEffect::GetSoundEffect() {
+	return _soundEffect;
+}
+		
+/**
+ * Action
+ *
+ *
+ *
+ *
+ *
+ */
+
+Action::Action(VisualEffect *ve, SoundDescriptor se, Actor *p, std::list<Actor *> args) {
+	_host = p;
+	_visualEffect = ve;
+	_soundEffect = se;
+	_arguments = args;
+}
+
+Action::~Action() {
+	delete _visualEffect;
+}
+
+Actor *Action::GetHost() {
+	return _host;
+}
+
+VisualEffect *Action::GetVisualEffect() {
+	return _visualEffect;
+}
+
+SoundDescriptor Action::GetSoundEffect() {
+	return _soundEffect;
+}
+
+std::list<Actor *> Action::GetArguments() {
+	return _arguments;
+}
+
+/**
+ * SkillAction
+ *
+ *
+ *
+ *
+ *
+ */
+
+SkillAction::SkillAction(GSkill *s, VisualEffect *ve, SoundDescriptor se, 
+				Actor *p, std::list<Actor *> args) :
+Action(ve, se, p, args)
+{
+	_skill = s;
+}
+
+void SkillAction::PerformAction() {
+	PerformSkill();
+}
+
+void SkillAction::PerformSkill() {
+	
+}
+
+/**
+ * SwapAction
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+ 
+SwapAction::SwapAction(VisualEffect *ve, SoundDescriptor se, 
+				Actor *p, std::list<Actor *> args) :
+Action(ve, se, p, args)
+{
+}
+
+void SwapAction::PerformAction() {
+	//here, we must tell the battle mode to swap the characters
+}
+
+/**
+ * UseItemAction
+ *
+ *
+ *
+ *
+ *
+ */
+ 
+UseItemAction::UseItemAction(GItem *i, VisualEffect *ve, SoundDescriptor se, 
+				Actor *p, std::list<Actor *> args) :
+Action(ve, se, p, args)
+{
+	_item = i;
+}
+
+void UseItemAction::PerformAction() {
+
+}
+
+/**
+ * AilmentEffect
+ *
+ *
+ *
+ *
+ *
+ */
+ 
+ 
+
+/**
+ * BattleMode
+ *
+ *
+ *
+ *
+ *
+ */
+ 
+BattleMode::BattleMode() : _UserInterface(this) {
 	cerr << "BATTLE: BattleMode constructor invoked." << endl;
 	
 	// Load the map from the Lua data file
 	//DataManager->LoadMap(this, map_id);
 	
-	
 	// To Tim: This sets up a cooriate system where "0, 0" is the top left hand corner of the screen,
 	// extends to SCREEN_LENGTH and SCREEN_HEIGHT, and has 1 depth level (ie, it's 2D)
-	_m_iCounter = 0;
+	
+	PlayerActor *claudius = new PlayerActor(InstanceManager->GetCharacter(GLOBAL_CLAUDIUS), this, 0, 0);
+	_playerActors.push_back(claudius);
+	_PCsInBattle.push_back(claudius);
+	
+	GEnemy *e = new GEnemy();
+	EnemyActor *enemy = new EnemyActor(e, this, 1, 1);
+	_enemyActors.push_back(enemy);
+		
+	Reset();
 }
 
 
 BattleMode::~BattleMode() {
 	// Clean up any allocated music/images/sounds/whatever data here. Don't forget! ^_~
 	cerr << "BATTLE: BattleMode destructor invoked." << endl;
+	
+	//clean up our actors
+	std::list<PlayerActor *>::iterator pc_itr = _playerActors.begin();
+	std::list<EnemyActor *>::iterator enemy_itr = _enemyActors.begin();
+	
+	for(; pc_itr != _playerActors.end(); pc_itr++) {
+		delete *pc_itr;
+	}
+	for(; enemy_itr != _enemyActors.end(); enemy_itr++) {
+		delete *enemy_itr;
+	}
 }
 
 
@@ -108,18 +894,58 @@ void BattleMode::Update(uint32 time_elapsed) {
 	// nothing else should need to know about them.
 	
 	// time_elapsed tells us how long it's been since the last time this function was called.
-	++_m_iCounter;
-	cerr << _m_iCounter << endl;
 	
-	if (_m_iCounter >= 1000)
-		ModeManager->Pop();
+	//get input
+	std::cout << "Updating with DT of: " << time_elapsed << std::endl;
+	
+	//update our characters
+	std::list<PlayerActor *>::iterator pc_itr = _PCsInBattle.begin();
+	std::list<EnemyActor *>::iterator enemy_itr = _enemyActors.begin();
+	
+	//is the battle over?
+	if(pc_itr == _PCsInBattle.end() || enemy_itr == _enemyActors.end()) {
+		ShutDown();
+	}
+	
+	//update the PCs
+	for(; pc_itr != _PCsInBattle.end(); pc_itr++) {
+		(*pc_itr)->Update(time_elapsed);
+	}
+	//update the enemies
+	for(; enemy_itr != _enemyActors.end(); enemy_itr++) {
+		(*enemy_itr)->Update(time_elapsed);
+		(*enemy_itr)->DoAI(time_elapsed);
+	}
 }
 
+void BattleMode::ShutDown() {
+	ModeManager->Pop();
+}
 
 void BattleMode::Draw() {
 	// This function draws the next frame that will be displayed to the screen. Like Update(), you'll
 	// probably write several sub-functions to keep the size of this function manageable. 
+	DrawBackground();
+	DrawCharacters();
+}
 
+void BattleMode::DrawBackground() {
+	std::cout << "Draw the Background." << std::endl;
+}
+
+void BattleMode::DrawCharacters() {
+	//update our characters
+	std::list<PlayerActor *>::iterator pc_itr = _PCsInBattle.begin();
+	std::list<EnemyActor *>::iterator enemy_itr = _enemyActors.begin();
+	
+	//update the PCs
+	for(; pc_itr != _PCsInBattle.end(); pc_itr++) {
+		(*pc_itr)->Draw();
+	}
+	//update the enemies
+	for(; enemy_itr != _enemyActors.end(); enemy_itr++) {
+		(*enemy_itr)->Draw();
+	}
 }
 
 } // namespace hoa_battle
