e9c948bf5d894c395acee2f237b3ebd8f92ddc79
visage <visage@localhost>
2006-05-18 01:16:33 +0000

    Added enemies information

---
+++ module_00/dat/enemies/skeleton.lua
@@ -0,0 +1,18 @@
+id = 1
+width = 64
+height = 64
+number_of_skills = 0
+movement_speed = 5
+base_hit_points = 10
+base_skill_points = 10
+base_experience_points = 5
+base_strength = 10
+base_intelligence = 10
+base_agility = 10
+growth_hit_points = 5
+growth_skill_points = 5
+growth_experience_points = 5
+growth_strength = 5
+growth_intelligence = 5
+growth_agility = 5
+	
