c200ef06d960b17cfda32b2645c23dd0c70da6e4
visage <visage@localhost>
2006-07-03 13:47:33 +0000

    Maps implemented.

--- module_00/dat/enemies/slime.lua
+++ module_00/dat/enemies/slime.lua
@@ -1,18 +1,26 @@
-id = 1
-width = 64
-height = 64
-number_of_skills = 0
-movement_speed = 5
-base_hit_points = 10
-base_skill_points = 10
-base_experience_points = 5
-base_strength = 10
-base_intelligence = 10
-base_agility = 10
-growth_hit_points = 5
-growth_skill_points = 5
-growth_experience_points = 5
-growth_strength = 5
-growth_intelligence = 5
-growth_agility = 5
-	
+id = 1;
+width = 64;
+height = 64;
+number_of_skills = 0;
+movement_speed = 5;
+base_hit_points = 10;
+base_skill_points = 10;
+base_experience_points = 5;
+base_strength = 10;
+base_intelligence = 10;
+base_agility = 10;
+growth_hit_points = 5;
+growth_skill_points = 5;
+growth_experience_points = 5;
+growth_strength = 5;
+growth_intelligence = 5;
+growth_agility = 5;
+
+
+number_of_maps = 2;
+map_x_1 = 32;
+map_y_1 = 64;
+map_name_1 = "Head";
+map_x_2 = 32;
+map_y_2 = 32;
+map_name_2 = "Chest";
