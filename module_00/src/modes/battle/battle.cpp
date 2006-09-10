ec3813876146ccb716a5434b4a283d473d67624f
gorzuate <gorzuate@localhost>
2006-09-10 04:01:51 +0000

    Added gaussian RV to damage calculations, used EnableGreyScale in battles

--- module_00/src/modes/battle/battle.cpp
+++ module_00/src/modes/battle/battle.cpp
@@ -71,7 +71,7 @@ ScriptEvent::~ScriptEvent()
 void ScriptEvent::RunScript() {
 	// TEMP: do basic damage to the actors
 	for (uint8 i = 0; i < _targets.size(); i++) {
-		_targets[i]->TEMP_Deal_Damage(rand() % 20);
+		_targets[i]->TEMP_Deal_Damage(GaussianRandomValue(12, 2));
 	}
 	// TODO: get script from global script repository and run, passing in list of arguments and host actor
 }
