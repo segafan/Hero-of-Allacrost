e5763d9aae442576e31aa3757afddec8ce79f076
gorzuate <gorzuate@localhost>
2006-09-24 19:06:49 +0000

    Cleaned up fade.h and fade.cpp.

--- demo/src/engine/video/fade.cpp
+++ demo/src/engine/video/fade.cpp
@@ -21,22 +21,21 @@ namespace hoa_video
 {
 
 //-----------------------------------------------------------------------------
-// FadeTo: Begins a fade to the given color in numSeconds
-//         returns true if invalid parameter is passed
+// FadeTo: Begins a fade to the given color in num_seconds
+//         returns false if invalid parameter is passed
 //-----------------------------------------------------------------------------
-
-bool ScreenFader::FadeTo(const Color &final, float numSeconds)
+bool ScreenFader::FadeTo(const Color &final, float num_seconds)
 {
-	if(numSeconds < 0.0f)
+	if(num_seconds < 0.0f)
 		return false;
 	
-	_initialColor = _currentColor;
-	_finalColor   = final;
+	_initial_color = _current_color;
+	_final_color   = final;
 	
-	_currentTime = 0;
-	_endTime     = int32(numSeconds * 1000);  // convert seconds to milliseconds here
+	_current_time = 0;
+	_end_time     = int32(num_seconds * 1000);  // convert seconds to milliseconds here
 	
-	_isFading = true;
+	_is_fading = true;
 	
 	// figure out if this is a simple fade or if an overlay is required
 	// A simple fade is defined as a fade from clear to black, from black
@@ -44,20 +43,20 @@ bool ScreenFader::FadeTo(const Color &final, float numSeconds)
 	// or black. More simply, it's a fade where both the initial and final
 	// color's RGB values are zeroed out
 
-	_useFadeOverlay = true;	
+	_use_fade_overlay = true;	
 
-	if( (_initialColor[0] == 0.0f &&
-	     _initialColor[1] == 0.0f &&
-	     _initialColor[2] == 0.0f &&
-	     _finalColor[0]   == 0.0f &&
-	     _finalColor[1]   == 0.0f &&
-	     _finalColor[2]   == 0.0f))
+	if( (_initial_color[0] == 0.0f &&
+	     _initial_color[1] == 0.0f &&
+	     _initial_color[2] == 0.0f &&
+	     _final_color[0]   == 0.0f &&
+	     _final_color[1]   == 0.0f &&
+	     _final_color[2]   == 0.0f))
 	{
-		_useFadeOverlay = false;
+		_use_fade_overlay = false;
 	}
 	else
 	{
-		_fadeModulation = 1.0f;
+		_fade_modulation = 1.0f;
 	}
 	
 	Update(0);  // do initial update
@@ -70,89 +69,87 @@ bool ScreenFader::FadeTo(const Color &final, float numSeconds)
 // Update: updates screen fader- figures out new interpolated fade color,
 //         whether to fade using overlays or modulation, etc.
 //-----------------------------------------------------------------------------
-
 bool ScreenFader::Update(int32 t)
 {
-	if(!_isFading)
+	if(!_is_fading)
 		return true;
 				
-	if(_currentTime >= _endTime)
+	if(_current_time >= _end_time)
 	{
-		_currentColor   = _finalColor;
-		_isFading       = false;
+		_current_color = _final_color;
+		_is_fading     = false;
 		
-		if(_useFadeOverlay)
+		if(_use_fade_overlay)
 		{
 			// check if we have faded to black or clear. If so, we can use modulation
-			if(_finalColor[3] == 0.0f ||
-			  (_finalColor[0] == 0.0f &&
-			   _finalColor[1] == 0.0f &&
-			   _finalColor[2] == 0.0f))
+			if(_final_color[3] == 0.0f ||
+			  (_final_color[0] == 0.0f &&
+			   _final_color[1] == 0.0f &&
+			   _final_color[2] == 0.0f))
 			{
-				_useFadeOverlay = false;
-				_fadeModulation = 1.0f - _finalColor[3];
+				_use_fade_overlay = false;
+				_fade_modulation = 1.0f - _final_color[3];
 			}
 		}
 		else
-			_fadeModulation = 1.0f - _finalColor[3];
+			_fade_modulation = 1.0f - _final_color[3];
 	}
 	else
 	{
 		// calculate the new interpolated color
-		float a = (float)_currentTime / (float)_endTime;
+		float a = (float)_current_time / (float)_end_time;
 
-		_currentColor[3] = Lerp(a, _initialColor[3], _finalColor[3]);
+		_current_color[3] = Lerp(a, _initial_color[3], _final_color[3]);
 
-		
 		// if we are fading to or from clear, then only the alpha should get
 		// interpolated.
-		if(_finalColor[3] == 0.0f)
+		if(_final_color[3] == 0.0f)
 		{
-			_currentColor[0] = _initialColor[0];
-			_currentColor[1] = _initialColor[1];
-			_currentColor[2] = _initialColor[2];
+			_current_color[0] = _initial_color[0];
+			_current_color[1] = _initial_color[1];
+			_current_color[2] = _initial_color[2];
 		}
-		if(_initialColor[3] == 0.0f)
+		if(_initial_color[3] == 0.0f)
 		{
-			_currentColor[0] = _finalColor[0];
-			_currentColor[1] = _finalColor[1];
-			_currentColor[2] = _finalColor[2];
+			_current_color[0] = _final_color[0];
+			_current_color[1] = _final_color[1];
+			_current_color[2] = _final_color[2];
 		}
 		else
 		{
-			_currentColor[0] = Lerp(a, _initialColor[0], _finalColor[0]);
-			_currentColor[1] = Lerp(a, _initialColor[1], _finalColor[1]);
-			_currentColor[2] = Lerp(a, _initialColor[2], _finalColor[2]);
+			_current_color[0] = Lerp(a, _initial_color[0], _final_color[0]);
+			_current_color[1] = Lerp(a, _initial_color[1], _final_color[1]);
+			_current_color[2] = Lerp(a, _initial_color[2], _final_color[2]);
 		}
 		
-		if(!_useFadeOverlay)
-			_fadeModulation = 1.0f - _currentColor[3];
+		if(!_use_fade_overlay)
+			_fade_modulation = 1.0f - _current_color[3];
 		else
-			_fadeOverlayColor = _currentColor;
+			_fade_overlay_color = _current_color;
 	}
 
-	_currentTime += t;
+	_current_time += t;
 	return true;
 }
 
 
+
 //-----------------------------------------------------------------------------
-// FadeScreen: sets up a fade to the given color over "fadeTime" number of seconds
+// FadeScreen: sets up a fade to the given color over "fade_time" number of seconds
 //-----------------------------------------------------------------------------
-bool GameVideo::FadeScreen(const Color &color, float fadeTime)
+bool GameVideo::FadeScreen(const Color &color, float fade_time)
 {
-	return _fader.FadeTo(color, fadeTime);
+	return _fader.FadeTo(color, fade_time);
 }
+
 
 
 //-----------------------------------------------------------------------------
 // IsFading: returns true if screen is in the middle of a fade
 //-----------------------------------------------------------------------------
-
 bool GameVideo::IsFading()
 {
 	return _fader.IsFading();
 }
-
 
 }  // namespace hoa_video
