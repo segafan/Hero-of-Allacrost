0e9120727c5307bd2d6f80b6f4ffe7a38722b78d
rootslinux <rootslinux@localhost>
2007-03-20 01:25:24 +0000

    Cleaned up menu_window.cpp; updated minor problem with stamina icon images

--- demo/src/engine/video/menu_window.cpp
+++ demo/src/engine/video/menu_window.cpp
@@ -8,21 +8,24 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 #include <sstream>
+
 #include "utils.h"
 #include "video.h"
 #include "gui.h"
 #include "menu_window.h"
 
 using namespace std;
+using namespace hoa_utils;
 using namespace hoa_video;
 using namespace hoa_video::private_video;
-using hoa_utils::MakeUnicodeString;
-using hoa_utils::ustring;
 
-int32 MenuWindow::_current_menu_id = 0;
-map<int32, MenuWindow *> MenuWindow::_menu_map;
 
 namespace hoa_video {
+
+int32 MenuWindow::_current_menu_id = 0;
+map<int32, MenuWindow*> MenuWindow::_menu_map;
+
+
 
 MenuWindow::MenuWindow() :
 	_width(0),
@@ -42,7 +45,8 @@ MenuWindow::MenuWindow() :
 bool MenuWindow::Create(float w, float h, int32 visible_flags, int32 shared_flags) {
 	if (w <= 0 || h <= 0) {
 		if (VIDEO_DEBUG)
-			cerr << "VIDEO ERROR: MenuWindow::Create() failed because an invalid width or height was passed in: (width=" << w << ", height=" << h << ")" << endl;
+			cerr << "VIDEO WARNING: MenuWindow::Create() failed because an invalid width or height was passed in: "
+				<< "(width = " << w << ", height = " << h << ")" << endl;
 		return false;
 	}
 
@@ -51,14 +55,13 @@ bool MenuWindow::Create(float w, float h, int32 visible_flags, int32 shared_flag
 	_edge_visible_flags = visible_flags;
 	_edge_shared_flags = shared_flags;
 
-	if (!_RecreateImage()) {
+	if (_RecreateImage() == false) {
 		if (VIDEO_DEBUG)
-			cerr << "VIDEO ERROR: MenuWindow::Create() failed because _RecreateImage() returned false" << endl;
+			cerr << "VIDEO WARNING: MenuWindow::Create() failed because the window could not be recreated" << endl;
 		return false;
 	}
 
-	// Add the new menu to the menu map
-
+	// Add the new menu window to the menu map
 	_menu_map[_id] = this;
 	_initialized = IsInitialized(_initialization_errors);
 	return true;
@@ -67,15 +70,15 @@ bool MenuWindow::Create(float w, float h, int32 visible_flags, int32 shared_flag
 
 
 void MenuWindow::Destroy() {
-	// get rid of the entry in the std::map
-	map<int32, MenuWindow *>::iterator menuIterator = _menu_map.find(_id);
+	// Remove the entry in the std::map
+	map<int32, MenuWindow*>::iterator menu_iterator = _menu_map.find(_id);
 
-	if(menuIterator != _menu_map.end()) {
-		_menu_map.erase(menuIterator);
+	if (menu_iterator != _menu_map.end()) {
+		_menu_map.erase(menu_iterator);
 	}
 	else {
 		if(VIDEO_DEBUG)
-			cerr << "VIDEO ERROR: could not find menu in the menu map on MenuWindow::Destroy()!" << endl;
+			cerr << "VIDEO WARNING: MenuWindow::Destroy() could not find the menu map entry" << endl;
 	}
 
 	VideoManager->DeleteImage(_menu_image);
@@ -86,20 +89,17 @@ void MenuWindow::Destroy() {
 void MenuWindow::Update(uint32 frame_time) {
 	_current_time += frame_time;
 
-	if(_current_time >= VIDEO_MENU_SCROLL_TIME)
-	{
-		if(_window_state == VIDEO_MENU_STATE_SHOWING)
+	if (_current_time >= VIDEO_MENU_SCROLL_TIME) {
+		if (_window_state == VIDEO_MENU_STATE_SHOWING)
 			_window_state = VIDEO_MENU_STATE_SHOWN;
-		else if(_window_state == VIDEO_MENU_STATE_HIDING)
+		else if (_window_state == VIDEO_MENU_STATE_HIDING)
 			_window_state = VIDEO_MENU_STATE_HIDDEN;
 	}
 
-	if(_window_state == VIDEO_MENU_STATE_HIDDEN || _window_state == VIDEO_MENU_STATE_SHOWN)
-	{
-		if(_is_scissored == true)
-		{
-			float xBuffer = (_width - _inner_width) / 2;
-			float yBuffer = (_height - _inner_height) / 2;
+	if (_window_state == VIDEO_MENU_STATE_HIDDEN || _window_state == VIDEO_MENU_STATE_SHOWN) {
+		if (_is_scissored == true) {
+			float x_buffer = (_width - _inner_width) / 2;
+			float y_buffer = (_height - _inner_height) / 2;
 
 			float left, right, bottom, top;
 			left = 0.0f;
@@ -109,15 +109,15 @@ void MenuWindow::Update(uint32 frame_time) {
 
 			VideoManager->PushState();
 			VideoManager->SetDrawFlags(_xalign, _yalign, 0);
-			MenuWindow::CalculateAlignedRect(left, right, bottom, top);
+			CalculateAlignedRect(left, right, bottom, top);
 			VideoManager->PopState();
 
 			_scissor_rect = VideoManager->CalculateScreenRect(left, right, bottom, top);
 
-			_scissor_rect.left   += static_cast<int32> (xBuffer);
-			_scissor_rect.width  -= static_cast<int32> (xBuffer * 2);
-			_scissor_rect.top    += static_cast<int32> (yBuffer);
-			_scissor_rect.height -= static_cast<int32> (yBuffer * 2);
+			_scissor_rect.left   += static_cast<int32>(x_buffer);
+			_scissor_rect.width  -= static_cast<int32>(x_buffer * 2);
+			_scissor_rect.top    += static_cast<int32>(y_buffer);
+			_scissor_rect.height -= static_cast<int32>(y_buffer * 2);
 		}
 
 		_is_scissored = false;
@@ -126,25 +126,22 @@ void MenuWindow::Update(uint32 frame_time) {
 
 	_is_scissored = true;
 
-	// how much of the menu to draw
-	float drawPortion = 1.0f;
+	// Holds the amount of the window that should be drawn (1.0 == 100%)
+	float draw_percent = 1.0f;
 
-	if(_display_mode != VIDEO_MENU_INSTANT && _window_state != VIDEO_MENU_STATE_SHOWN)
-	{
-		float t = float(_current_time) / float(VIDEO_MENU_SCROLL_TIME);
-		if(t > 1.0f)
-			t = 1.0f;
+	if (_display_mode != VIDEO_MENU_INSTANT && _window_state != VIDEO_MENU_STATE_SHOWN) {
+		float time = static_cast<float>(_current_time) / static_cast<float>(VIDEO_MENU_SCROLL_TIME);
+		if (time > 1.0f)
+			time = 1.0f;
 
-		if(_window_state == VIDEO_MENU_STATE_HIDING)
-			t = 1.0f - t;
+		if (_window_state == VIDEO_MENU_STATE_HIDING)
+			time = 1.0f - time;
 
-		drawPortion = t;
+		draw_percent = time;
 	}
 
-	if(drawPortion != 1.0f)
-	{
-		if(_display_mode == VIDEO_MENU_EXPAND_FROM_CENTER)
-		{
+	if (draw_percent != 1.0f) {
+		if (_display_mode == VIDEO_MENU_EXPAND_FROM_CENTER) {
 			float left, right, bottom, top;
 			left = 0.0f;
 			right = _width;
@@ -153,27 +150,25 @@ void MenuWindow::Update(uint32 frame_time) {
 
 			VideoManager->PushState();
 			VideoManager->SetDrawFlags(_xalign, _yalign, 0);
-			MenuWindow::CalculateAlignedRect(left, right, bottom, top);
+			CalculateAlignedRect(left, right, bottom, top);
 			VideoManager->PopState();
 
 			float center = (top + bottom) * 0.5f;
 
-			bottom = center * (1.0f - drawPortion) + bottom * drawPortion;
-			top    = center * (1.0f - drawPortion) + top * drawPortion;
+			bottom = center * (1.0f - draw_percent) + bottom * draw_percent;
+			top    = center * (1.0f - draw_percent) + top * draw_percent;
 
 			_scissor_rect = VideoManager->CalculateScreenRect(left, right, bottom, top);
 		}
 	}
-
-	return;
-}
+} // void MenuWindow::Update(uint32 frame_time)
 
 
 
 void MenuWindow::Draw() {
-	if (!_initialized) {
+	if (_initialized == false) {
 		if (VIDEO_DEBUG)
-			cerr << "MenuWindow::Draw() failed because the menu window was not initialized:" << endl << _initialization_errors << endl;
+			cerr << "VIDEO WARNING: MenuWindow::Draw() failed because the menu window was not initialized:\n" << _initialization_errors << endl;
 		return;
 	}
 
@@ -181,8 +176,8 @@ void MenuWindow::Draw() {
 		return;
 
 	VideoManager->_PushContext();
-
 	VideoManager->SetDrawFlags(_xalign, _yalign, VIDEO_BLEND, 0);
+
 	if (_is_scissored) {
 		ScreenRect rect = _scissor_rect;
 		if (VideoManager->IsScissoringEnabled()) {
@@ -195,56 +190,43 @@ void MenuWindow::Draw() {
 	}
 
 	VideoManager->Move(_x_position, _y_position);
-	if (!VideoManager->DrawImage(_menu_image, Color::white)) {
-		if (VIDEO_DEBUG)
-			cerr << "MenuMode::Draw() failed because GameVideo::DrawImage() returned false!" << endl;
-		VideoManager->_PopContext();
-		return;
-	}
+	VideoManager->DrawImage(_menu_image, Color::white);
 
 	VideoManager->_PopContext();
 	return;
-}
+} // void MenuWindow::Draw()
 
 
 
-void MenuWindow::Show()
-{
-	// fail if menu window isn't initialized properly
-	if(!_initialized)
-	{
-		if(VIDEO_DEBUG)
-			cerr << "MenuWindow::Draw() failed because the menu window was not initialized:" << endl << _initialization_errors << endl;
+void MenuWindow::Show() {
+	if (_initialized == false) {
+		if (VIDEO_DEBUG)
+			cerr << "VIDEO WARNING: MenuWindow::Show() failed because the menu window was not initialized:\n" << _initialization_errors << endl;
 		return;
 	}
 
-	if(_window_state == VIDEO_MENU_STATE_SHOWING || _window_state == VIDEO_MENU_STATE_SHOWN)
-	{
-		// nothing to do
+	if (_window_state == VIDEO_MENU_STATE_SHOWING || _window_state == VIDEO_MENU_STATE_SHOWN) {
 		return;
 	}
 
 	_current_time = 0;
 
-	if(_display_mode == VIDEO_MENU_INSTANT)
+	if (_display_mode == VIDEO_MENU_INSTANT)
 		_window_state = VIDEO_MENU_STATE_SHOWN;
 	else
 		_window_state = VIDEO_MENU_STATE_SHOWING;
-
-	return;
-}
+} // void MenuWindow::Show()
 
 
 
 void MenuWindow::Hide() {
-	if (!_initialized) {
+	if (_initialized == false) {
 		if (VIDEO_DEBUG)
-			cerr << "MenuWindow::Draw() failed because the menu window was not initialized:" << endl << _initialization_errors << endl;
+			cerr << "VIDEO WARNING: MenuWindow::Hide() failed because the menu window was not initialized:\n" << _initialization_errors << endl;
 		return;
 	}
 
 	if (_window_state == VIDEO_MENU_STATE_HIDING || _window_state == VIDEO_MENU_STATE_HIDDEN) {
-		// Nothing to do
 		return;
 	}
 
@@ -254,39 +236,45 @@ void MenuWindow::Hide() {
 		_window_state = VIDEO_MENU_STATE_HIDDEN;
 	else
 		_window_state = VIDEO_MENU_STATE_HIDING;
-}
+} // void MenuWindow::Hide()
 
 
 
-bool MenuWindow::IsInitialized(string &errors) {
-	bool success = true;
-
+bool MenuWindow::IsInitialized(string& errors) {
 	errors.clear();
-	ostringstream s;
+	ostringstream stream;
 
-	// check width
-	if(_width <= 0.0f || _width > 1024.0f)
-		s << "* Invalid width (" << _width << ")" << endl;
+	// Check width
+	if (_width <= 0.0f || _width > 1024.0f)
+		stream << "* Invalid width (" << _width << ")" << endl;
 
-	// check height
-	if(_height <= 0.0f || _height > 768.0f)
-		s << "* Invalid height (" << _height << ")" << endl;
+	// Check height
+	if (_height <= 0.0f || _height > 768.0f)
+		stream << "* Invalid height (" << _height << ")" << endl;
 
-	// check display mode
-	if(_display_mode <= VIDEO_MENU_INVALID || _display_mode >= VIDEO_MENU_TOTAL)
-		s << "* Invalid display mode (" << _display_mode << ")" << endl;
+	// Check display mode
+	if (_display_mode <= VIDEO_MENU_INVALID || _display_mode >= VIDEO_MENU_TOTAL)
+		stream << "* Invalid display mode (" << _display_mode << ")" << endl;
 
-	// check state
-	if(_window_state <= VIDEO_MENU_STATE_INVALID || _window_state >= VIDEO_MENU_STATE_TOTAL)
-		s << "* Invalid state (" << _window_state << ")" << endl;
+	// Check state
+	if (_window_state <= VIDEO_MENU_STATE_INVALID || _window_state >= VIDEO_MENU_STATE_TOTAL)
+		stream << "* Invalid state (" << _window_state << ")" << endl;
 
-	// check to see a valid image is loaded
-	if(_menu_image.GetWidth() == 0)
-		s << "* Menu image is not loaded" << endl;
+	// Check to see a valid image is loaded
+	if (_menu_image.GetWidth() == 0)
+		stream << "* Menu image is not loaded" << endl;
 
-	_initialized = success;
-	return success;
-}
+	errors = stream.str();
+
+	if (errors.empty()) {
+		_initialized = true;
+	}
+	else {
+		_initialized = false;
+	}
+
+	return _initialized;
+} // bool MenuWindow::IsInitialized(string& errors)
 
 
 
@@ -304,10 +292,10 @@ void MenuWindow::ChangeEdgeSharedFlags(int32 flags) {
 
 
 
-void MenuWindow::SetDisplayMode(MenuDisplayMode mode) {
+void MenuWindow::SetDisplayMode(VIDEO_MENU_DISPLAY_MODE mode) {
 	if (mode <= VIDEO_MENU_INVALID || mode >= VIDEO_MENU_TOTAL) {
-		if(VIDEO_DEBUG)
-			cerr << "VIDEO ERROR: MenuWindow::SetDisplayMode() failed because an invalid mode (" << mode << ") was passed in!" << endl;
+		if (VIDEO_DEBUG)
+			cerr << "VIDEO WARNING: MenuWindow::SetDisplayMode() failed because an invalid argument was given: " << mode << endl;
 		return;
 	}
 
