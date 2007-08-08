4ca9ab0a3f3620cb99c53b77a21c649f4a7d1281
jetryl <jetryl@localhost>
2007-08-08 06:44:13 +0000

    Added exception handling to main.cpp.

--- demo/src/main.cpp
+++ demo/src/main.cpp
@@ -101,157 +101,152 @@ void QuitAllacrost() {
 *** \return True if the game engine was initialized successfully, false if an unrecoverable error occured
 ***
 **/
-bool InitializeEngine() {
+void InitializeEngine() throw (Exception) {
 	// Initialize SDL. The video, audio, and joystick subsystems are initialized elsewhere.
-	if (SDL_Init(SDL_INIT_TIMER) != 0) {
-		cerr << "MAIN ERROR: Unable to initialize SDL: " << SDL_GetError() << endl;
-		return false;
-	}
+      	if (SDL_Init(SDL_INIT_TIMER) != 0) {
+      		throw Exception("MAIN ERROR: Unable to initialize SDL: ", __FILE__, __LINE__, __func__);
+      	}
+      
+      	// Create and initialize singleton class managers
+      	AudioManager = GameAudio::SingletonCreate();
+      	InputManager = GameInput::SingletonCreate();
+      	VideoManager = GameVideo::SingletonCreate();
+      	ScriptManager = GameScript::SingletonCreate();
+      	SystemManager = GameSystem::SingletonCreate();
+      	ModeManager = GameModeManager::SingletonCreate();
+      	GlobalManager = GameGlobal::SingletonCreate();
+      
+      	if (VideoManager->SingletonInitialize() == false) {
+      		throw Exception("ERROR: unable to initialize VideoManager", __FILE__, __LINE__, __func__);
+      	}
+      
+      	if (VideoManager->LoadMenuSkin("black_sleet", "img/menus/black_sleet_skin.png", "img/menus/black_sleet_texture.png") == false) {
+      		  throw Exception("Failed to load the 'Black Sleet' MenuSkin images.", __FILE__, __LINE__, __func__);
+      	}
+      
+      	if (VideoManager->LoadFont("img/fonts/vtc_switchblade_romance.ttf", "default", 18) == false) {
+      		  throw Exception("Failed to load the 'Switchblade Romance' font as 'default, size 18'", __FILE__, __LINE__, __func__);
+      	}
+      
+      	VideoManager->SetFontShadowOffsets("default", 1, -2);
+      	VideoManager->SetFontShadowStyle("default", VIDEO_TEXT_SHADOW_BLACK);
+      
+      	if (!VideoManager->LoadFont("img/fonts/vtc_switchblade_romance.ttf", "map", 24)) {
+      		  throw Exception("Failed to load the 'Switchblade Romance' font as 'map, size 24'", __FILE__, __LINE__, __func__);
+      	}
+      
+      	VideoManager->SetFontShadowOffsets("map", 0, 0);
+      	VideoManager->SetFontShadowStyle("map", VIDEO_TEXT_SHADOW_BLACK);
+      
+      	if (!VideoManager->LoadFont("img/fonts/vtc_switchblade_romance.ttf", "battle", 20)) {
+      		  throw Exception("Failed to load the 'Switchblade Romance' font as 'battle, size 20'", __FILE__, __LINE__, __func__);
+       	}
+      
+      	VideoManager->SetFontShadowOffsets("battle", 1, -2);
+      	VideoManager->SetFontShadowStyle("battle", VIDEO_TEXT_SHADOW_BLACK);
+      
+      	// Font used to show damage received / given in battle mode
+      	if (!VideoManager->LoadFont("img/fonts/vtc_switchblade_romance.ttf", "battle_dmg", 24)) {
+      		  throw Exception("Failed to load the 'Switchblade Romance' font as 'battle_dmg, size 24'", __FILE__, __LINE__, __func__);
+       	}
+      
+      	VideoManager->SetFontShadowOffsets("battle_dmg", 1, -2);
+      	VideoManager->SetFontShadowStyle("battle_dmg", VIDEO_TEXT_SHADOW_BLACK);
+      
+      	if (AudioManager->SingletonInitialize() == false) {
+      			throw Exception("ERROR: unable to initialize AudioManager", __FILE__, __LINE__, __func__);
+      	}
+      
+      	if (ScriptManager->SingletonInitialize() == false) {
+      			throw Exception("ERROR: unable to initialize ScriptManager", __FILE__, __LINE__, __func__);
+      	}
+      	hoa_defs::BindEngineToLua();
+      
+      	if (SystemManager->SingletonInitialize() == false) {
+      			throw Exception("ERROR: unable to initialize SystemManager", __FILE__, __LINE__, __func__);
+      	}
+      	if (InputManager->SingletonInitialize() == false) {
+      			throw Exception("ERROR: unable to initialize InputManager", __FILE__, __LINE__, __func__);
+      	}
+      	if (GlobalManager->SingletonInitialize() == false) {
+      			throw Exception("ERROR: unable to initialize GlobalManager", __FILE__, __LINE__, __func__);
+      	}
+      	if (ModeManager->SingletonInitialize() == false) {
+      			throw Exception("ERROR: unable to initialize ModeManager", __FILE__, __LINE__, __func__);
+      	}
+      
+      	// Set the window title and icon name
+      	SDL_WM_SetCaption("Hero of Allacrost", "Hero of Allacrost");
+      
+      	// Set the window icon
+      	#ifdef _WIN32
+      		SDL_WM_SetIcon(SDL_LoadBMP("img/logos/program_icon.bmp"), NULL);
+      	#else
+      		// Later, add an icon here for non-Windows systems (which support more than 32x32 .bmp files)
+      		SDL_WM_SetIcon(SDL_LoadBMP("img/logos/program_icon.bmp"), NULL);
+      	#endif
+      
+      	// Hide the mouse cursor since we don't use or acknowledge mouse input from the user
+      	SDL_ShowCursor(SDL_DISABLE);
+      
+      	// Enabled for multilingual keyboard support
+      	SDL_EnableUNICODE(1);
+      
+      	// Ignore the events that we don't care about so they never appear in the event queue
+      	SDL_EventState(SDL_MOUSEMOTION, SDL_IGNORE);
+      	SDL_EventState(SDL_MOUSEBUTTONDOWN, SDL_IGNORE);
+      	SDL_EventState(SDL_MOUSEBUTTONUP, SDL_IGNORE);
+      	SDL_EventState(SDL_SYSWMEVENT, SDL_IGNORE);
+      	SDL_EventState(SDL_VIDEOEXPOSE, SDL_IGNORE);
+      	SDL_EventState(SDL_USEREVENT, SDL_IGNORE);
+      
+      	SystemManager->InitializeTimers();
+      
+      } // void InitializeEngine()
+      
+  // Every great game begins with a single function :)
+  int32 main(int32 argc, char *argv[]) {
+  	// When the program exits, first the QuitAllacrost() function is called, followed by SDL_Quit()
+  	atexit(SDL_Quit);
+  	atexit(QuitAllacrost);
+      
+      try {
+      	// Change to the directory where the Allacrost data is stored
+      	#ifdef __MACH__
+      		string path;
+      		path = argv[0];
+      		// Remove the binary name
+      		path.erase(path.find_last_of('/'));
+      		// Remove the MacOS directory
+      		path.erase(path.find_last_of('/'));
+      		// Now the program should be in app/Contents
+      		path.append ("/Resources/");
+      		chdir(path.c_str());
+      	#elif defined(__linux__) || defined(__FreeBSD__)
+      		// Look for data files in DATADIR only if they are not available in the
+      		// current directory.
+      		if (ifstream("dat/config/settings.lua") == NULL)
+      			chdir(DATADIR);
+      	#endif
+      
+      	// Initialize the random number generator (note: 'unsigned int' is a required usage in this case)
+      	srand(static_cast<unsigned int>(time(NULL)));
+      
+      	// This variable will be set by the ParseProgramOptions function
+      	int32 return_code = EXIT_FAILURE;
+      
+      	// Parse command lines and exit out of the game if needed
+      	if (hoa_main::ParseProgramOptions(return_code, argc, argv) == false) {
+      		return return_code;
+      	}
+      
+      	InitializeEngine(); //<-- any errors in this get caught by exceptions
+		
+    } catch (Exception& e) {
+        cerr << e.ToString() << std::endl;
+        return EXIT_FAILURE;
+    }
 
-	// Create and initialize singleton class managers
-	AudioManager = GameAudio::SingletonCreate();
-	InputManager = GameInput::SingletonCreate();
-	VideoManager = GameVideo::SingletonCreate();
-	ScriptManager = GameScript::SingletonCreate();
-	SystemManager = GameSystem::SingletonCreate();
-	ModeManager = GameModeManager::SingletonCreate();
-	GlobalManager = GameGlobal::SingletonCreate();
-
-	if (VideoManager->SingletonInitialize() == false) {
-		cerr << "ERROR: unable to initialize VideoManager" << endl;
-		return false;
-	}
-
-	if (VideoManager->LoadMenuSkin("black_sleet", "img/menus/black_sleet_skin.png", "img/menus/black_sleet_texture.png") == false) {
-		return false;
-	}
-
-	if (VideoManager->LoadFont("img/fonts/vtc_switchblade_romance.ttf", "default", 18) == false) {
-		return false;
-	}
-
-	VideoManager->SetFontShadowOffsets("default", 1, -2);
-	VideoManager->SetFontShadowStyle("default", VIDEO_TEXT_SHADOW_BLACK);
-
-	if (!VideoManager->LoadFont("img/fonts/vtc_switchblade_romance.ttf", "map", 24)) {
-		return false;
-	}
-
-	VideoManager->SetFontShadowOffsets("map", 0, 0);
-	VideoManager->SetFontShadowStyle("map", VIDEO_TEXT_SHADOW_BLACK);
-
-	if (!VideoManager->LoadFont("img/fonts/vtc_switchblade_romance.ttf", "battle", 20)) {
-		return false;
-	}
-
-	VideoManager->SetFontShadowOffsets("battle", 1, -2);
-	VideoManager->SetFontShadowStyle("battle", VIDEO_TEXT_SHADOW_BLACK);
-
-	// Font used to show damage received / given in battle mode
-	if (!VideoManager->LoadFont("img/fonts/vtc_switchblade_romance.ttf", "battle_dmg", 24)) {
-		return false;
-	}
-
-	VideoManager->SetFontShadowOffsets("battle_dmg", 1, -2);
-	VideoManager->SetFontShadowStyle("battle_dmg", VIDEO_TEXT_SHADOW_BLACK);
-
-	if (AudioManager->SingletonInitialize() == false) {
-		cerr << "ERROR: unable to initialize AudioManager" << endl;
-		return false;
-	}
-
-	if (ScriptManager->SingletonInitialize() == false) {
-		cerr << "ERROR: unable to initialize ScriptManager" << endl;
-		return false;
-	}
-	hoa_defs::BindEngineToLua();
-
-	if (SystemManager->SingletonInitialize() == false) {
-		cerr << "ERROR: unable to initialize SystemManager" << endl;
-		return false;
-	}
-	if (InputManager->SingletonInitialize() == false) {
-		cerr << "ERROR: unable to initialize InputManager" << endl;
-		return false;
-	}
-	if (GlobalManager->SingletonInitialize() == false) {
-		cerr << "ERROR: unable to initialize GlobalManager" << endl;
-		return false;
-	}
-	if (ModeManager->SingletonInitialize() == false) {
-		cerr << "ERROR: unable to initialize ModeManager" << endl;
-		return false;
-	}
-
-	// Set the window title and icon name
-	SDL_WM_SetCaption("Hero of Allacrost", "Hero of Allacrost");
-
-	// Set the window icon
-	#ifdef _WIN32
-		SDL_WM_SetIcon(SDL_LoadBMP("img/logos/program_icon.bmp"), NULL);
-	#else
-		// Later, add an icon here for non-Windows systems (which support more than 32x32 .bmp files)
-		SDL_WM_SetIcon(SDL_LoadBMP("img/logos/program_icon.bmp"), NULL);
-	#endif
-
-	// Hide the mouse cursor since we don't use or acknowledge mouse input from the user
-	SDL_ShowCursor(SDL_DISABLE);
-
-	// Enabled for multilingual keyboard support
-	SDL_EnableUNICODE(1);
-
-	// Ignore the events that we don't care about so they never appear in the event queue
-	SDL_EventState(SDL_MOUSEMOTION, SDL_IGNORE);
-	SDL_EventState(SDL_MOUSEBUTTONDOWN, SDL_IGNORE);
-	SDL_EventState(SDL_MOUSEBUTTONUP, SDL_IGNORE);
-	SDL_EventState(SDL_SYSWMEVENT, SDL_IGNORE);
-	SDL_EventState(SDL_VIDEOEXPOSE, SDL_IGNORE);
-	SDL_EventState(SDL_USEREVENT, SDL_IGNORE);
-
-	SystemManager->InitializeTimers();
-
-	return true;
-} // bool InitializeEngine()
-
-// Every great game begins with a single function :)
-int32 main(int32 argc, char *argv[]) {
-	// When the program exits, first the QuitAllacrost() function is called, followed by SDL_Quit()
-	atexit(SDL_Quit);
-	atexit(QuitAllacrost);
-
-	// Change to the directory where the Allacrost data is stored
-	#ifdef __MACH__
-		string path;
-		path = argv[0];
-		// Remove the binary name
-		path.erase(path.find_last_of('/'));
-		// Remove the MacOS directory
-		path.erase(path.find_last_of('/'));
-		// Now the program should be in app/Contents
-		path.append ("/Resources/");
-		chdir(path.c_str());
-	#elif defined(__linux__) || defined(__FreeBSD__)
-		// Look for data files in DATADIR only if they are not available in the
-		// current directory.
-		if (ifstream("dat/config/settings.lua") == NULL)
-			chdir(DATADIR);
-	#endif
-
-	// Initialize the random number generator (note: 'unsigned int' is a required usage in this case)
-	srand(static_cast<unsigned int>(time(NULL)));
-
-	// This variable will be set by the ParseProgramOptions function
-	int32 return_code = EXIT_FAILURE;
-
-	// Parse command lines and exit out of the game if needed
-	if (hoa_main::ParseProgramOptions(return_code, argc, argv) == false) {
-		return return_code;
-	}
-
-	if (InitializeEngine() == false) {
-		cerr << "ERROR: failed to initialize game engine, exiting..." << endl;
-		return EXIT_FAILURE;
-	}
 
     try {
         // This is the main loop for the game. The loop iterates once for every frame drawn to the screen.
@@ -273,7 +268,7 @@ int32 main(int32 argc, char *argv[]) {
             // 5) Update the game status
             ModeManager->Update();
         } // while (SystemManager->NotDone())
-    } catch (Exception e) {
+    } catch (Exception& e) {
         cerr << e.ToString() << std::endl;
         return EXIT_FAILURE;
     }
