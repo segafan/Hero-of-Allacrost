5d9896303fc0b2746c0b3f67eb98e4d388d31d5c
blackkknight <blackkknight@localhost>
2007-10-25 16:46:41 +0000

    Added grid to tileset editor.

--- demo/src/editor/tileset_editor.cpp
+++ demo/src/editor/tileset_editor.cpp
@@ -19,23 +19,73 @@ using namespace hoa_script;
 using namespace hoa_video;
 
 
+
+//! OverlayGrid Implementation
+OverlayGrid::OverlayGrid()
+{
+	tileset = new Tileset;		
+	tileset->tiles.resize(1);
+}
+
+OverlayGrid::~OverlayGrid()
+{
+	delete tileset;
+}
+void OverlayGrid::paintGL()
+{
+	VideoManager->SetCoordSys(0.0f, VideoManager->GetScreenWidth() / TILE_WIDTH,
+		VideoManager->GetScreenHeight() / TILE_HEIGHT, 0.0f);
+	VideoManager->SetDrawFlags(VIDEO_X_LEFT, VIDEO_Y_TOP, VIDEO_BLEND, 0);
+	VideoManager->Clear(Color::white);
+	
+	
+	
+	
+	
+	
+	tileset->tiles[0].Draw();
+	
+	
+	
+	
+	
+	
+	VideoManager->DrawGrid(0.0f, 0.0f, 0.5f, 0.5f, Color::black);
+
+}
+void OverlayGrid::resizeGL(int w,int h)
+{
+	VideoManager->SetResolution(512, 512);
+	VideoManager->ApplySettings();
+}
+void OverlayGrid::initializeGL()
+{
+	VideoManager->SetTarget(VIDEO_TARGET_QT_WIDGET);
+	VideoManager->SingletonInitialize();
+	VideoManager->ToggleFPS();
+}
+
+
+//! TilesetEditor Implementation
+
 TilesetEditor::TilesetEditor(QWidget* parent,const QString& name,bool prop)
 : QDialog(parent, (const char*) name)
 {
 	setCaption("Tileset Definition File Editor");
-	
-	//Create current tileset
-	_current_tileset = new Tileset;	
 	
 	//Create GUI Items
 	_opentileset_pbut = new QPushButton("Open",this);
 	_cancel_pbut   = new QPushButton("Cancel", this);
 	_ok_pbut       = new QPushButton("OK", this);
 	_cancel_pbut->setDefault(true);
-	_tileset_label = new QLabel("Open a tileset...",this);
-
-	_tileset_label->resize(512,512);
 	
+
+	//Create the overlay grid
+	_walkability_grid = new OverlayGrid;
+	_walkability_grid->resize(512,512);
+	_walkability_grid->setFixedWidth(512);
+	_walkability_grid->setFixedHeight(512);
+
 	// connect button signals
 	connect(_ok_pbut,     SIGNAL(released()), this, SLOT(accept()));
 	connect(_cancel_pbut, SIGNAL(released()), this, SLOT(reject()));
@@ -47,10 +97,9 @@ TilesetEditor::TilesetEditor(QWidget* parent,const QString& name,bool prop)
 	_dia_layout->addWidget(_opentileset_pbut, 0, 1);
 	_dia_layout->addWidget(_ok_pbut, 1, 1);
 	_dia_layout->addWidget(_cancel_pbut, 2, 1);
-	_dia_layout->addWidget(_tileset_label,0,0,3,1);
+	_dia_layout->addWidget(_walkability_grid,0,0,3,1);
 	
 	
-
 }
 
 TilesetEditor::~TilesetEditor()
@@ -60,12 +109,11 @@ TilesetEditor::~TilesetEditor()
 	delete _ok_pbut;
 	delete _dia_layout;
 
-	delete _current_tileset;
+	delete _walkability_grid;
 }
 
 void TilesetEditor::_openTDF()
 {
-
 	//get the filename to open through the OpenFileName dialog
 	QString file_name = QFileDialog::getOpenFileName(this, "HoA Level Editor -- File Open",
 		"dat/tilesets", "Tilesets (*.lua)");
@@ -80,30 +128,17 @@ void TilesetEditor::_openTDF()
 	
 
 	// Load the tileset image.
-	_current_tileset->tiles.resize(1);
+	_walkability_grid->tileset->tiles.resize(1);
 	
-	_current_tileset->tiles[0].SetDimensions(16.0f, 16.0f);
-	if (ImageDescriptor::LoadMultiImageFromElementGrid(_current_tileset->tiles, std::string(img_filename.toAscii()), 1, 1) == false)
+	_walkability_grid->tileset->tiles[0].SetDimensions(16.0f, 16.0f);
+	if (ImageDescriptor::LoadMultiImageFromElementGrid(_walkability_grid->tileset->tiles, std::string(img_filename.toAscii()), 1, 1) == false)
 		qDebug("LoadMultiImage failed to load tileset " + img_filename);
 	
 	
-	QRect rectangle;
-	QImageReader reader(img_filename, "png");
-	rectangle.setRect(0, 0, 512, 512);
-	reader.setClipRect(rectangle);
-	QImage tile_img = reader.read();
-	QVariant variant = tile_img;
-	if (!tile_img.isNull())
-	{
-		QPixmap tile_pixmap = variant.value<QPixmap>();
-		_tileset_label->setPixmap(tile_pixmap);
-	} // image of the tile must not be null
-	else
-		qDebug(QString("%1").arg(reader.error()));
-
-
+	
 	//TODO : Load the tileset definition file
 	//walkability autotiling etc.
 }
+
 
 
