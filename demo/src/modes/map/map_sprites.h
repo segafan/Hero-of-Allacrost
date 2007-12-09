e74c96ab66df3867ae9facf445c958429bc2c7c5
gorzuate <gorzuate@localhost>
2007-12-09 05:19:28 +0000

    Tweaked NPCs speeds, positions in demo_town.

--- demo/src/modes/map/map_sprites.h
+++ demo/src/modes/map/map_sprites.h
@@ -36,11 +36,11 @@ namespace private_map {
 *** the distance of one map grid (16 pixels).
 **/
 //@{
-const float VERY_SLOW_SPEED = 200.0f;
-const float SLOW_SPEED      = 175.0f;
+const float VERY_SLOW_SPEED = 225.0f;
+const float SLOW_SPEED      = 190.0f;
 const float NORMAL_SPEED    = 150.0f;
-const float FAST_SPEED      = 125.0f;
-const float VERY_FAST_SPEED = 100.0f;
+const float FAST_SPEED      = 110.0f;
+const float VERY_FAST_SPEED = 75.0f;
 //@}
 
 /** \name Sprite Direction Constants
