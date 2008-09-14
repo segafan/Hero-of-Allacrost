b74b95d34a66c5c7c8f41aa0c943de5429eb8a8f
gorzuate <gorzuate@localhost>
2008-09-14 18:47:20 +0000

    Changed SDL_endian.h to SDL/SDL_endian.h.

--- demo/src/engine/audio/audio_input.cpp
+++ demo/src/engine/audio/audio_input.cpp
@@ -21,7 +21,7 @@
 *** ***************************************************************************/
 
 #include "audio_input.h"
-#include <SDL_endian.h>
+#include <SDL/SDL_endian.h>
 
 using namespace std;
 
