8e4c586ec7d5f3b9a93f40496ec52d0238173573
wk2 <wk2@localhost>
2009-06-10 11:04:29 +0000

    * Update and Draw functions are now optional for map files. * map no longer have to specify map_functions[0] = function() return true end in map files * Added help functions LoadNewMap and LoadNewShop to global.lua

--- demo/dat/global.lua
+++ demo/dat/global.lua
@@ -3,8 +3,35 @@ LAILA = 2
 
 function NewGame()
 	GlobalManager:AddCharacter(CLAUDIUS);
---	GlobalManager:GetCharacter(CLAUDIUS):AddSkill(10001);
 	GlobalManager:SetDrunes(250);
 	GlobalManager:AddToInventory(1, 2);
 	GlobalManager:SetLocation("dat/maps/demo_town.lua");
+end
+
+
+-- Helper functions
+
+function LoadNewMap(map_name)
+	ModeManager:Pop();
+	local cave_map = hoa_map.MapMode("dat/maps/" .. map_name .. ".lua");
+	ModeManager:Push(cave_map);
+end
+
+function LoadNewShop(...)
+	local i, v;
+	local shop = hoa_shop.ShopMode();
+	for i,v in ipairs(arg) do
+		shop:AddObject(v);
+	end
+	ModeManager:Push(shop);
+end
+
+
+-- Dummy functions
+
+enemy_ids = {}
+map_functions = {}
+
+map_functions[0] = function()
+	return true;
 end
