16ae8a6d29362a1d70ccb10d67ddd46cc3a09779
prophile <prophile@localhost>
2010-01-25 00:37:54 +0000

    Updated the copyright date on all source files.
    For future reference, the command I used for recursive find and replace was:
      find . ! -regex ".*.svn.*" -type f -exec grep -l "2004-2008" {} \; | xargs sed -i "" "s/2004-2008/2004-2010/"

--- demo/src/editor/editor.cpp
+++ demo/src/editor/editor.cpp
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////////
-//            Copyright (C) 2004-2008 by The Allacrost Project
+//            Copyright (C) 2004-2010 by The Allacrost Project
 //                         All Rights Reserved
 //
 // This code is licensed under the GNU GPL version 2. It is free software
@@ -1008,7 +1008,7 @@ void Editor::_HelpAbout()
     QMessageBox::about(this, "HoA Level Editor -- About",
 		"<center><h1><font color=blue>Hero of Allacrost Level Editor<font>"
 		"</h1></center>"
-		"<center><h2><font color=blue>Copyright (c) 2004-2008<font></h2></center>"
+		"<center><h2><font color=blue>Copyright (c) 2004-2010<font></h2></center>"
 		"<p>A level editor created for the Hero of Allacrost project."
 		" See 'http://www.allacrost.org/' for more details</p>");
 } // _HelpAbout()
