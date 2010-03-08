b20e0bd44730596c5ce7f27b145e4a2963ab57fb
rootslinux <rootslinux@localhost>
2010-03-08 07:06:35 +0000

    Debug options from boot menu are now configured in boot.lua instead of boot.cpp

--- demo/dat/config/boot.lua
+++ demo/dat/config/boot.lua
@@ -41,3 +41,62 @@ sound_files = { "snd/confirm.wav",
                 "snd/volume_test.wav" };
 
 -- -------------------------------------------------- --
+
+function BootBattleTest()
+	print("BootBattleTest");
+
+	GlobalManager:AddCharacter(1);
+	GlobalManager:AddCharacter(2);
+	GlobalManager:AddCharacter(4);
+	GlobalManager:AddCharacter(8);
+	
+	local battle = hoa_battle.BattleMode();
+	battle:AddEnemy(1);
+	battle:AddEnemy(1);
+	battle:AddEnemy(1);
+	battle:AddEnemy(1);
+	battle:AddEnemy(1);
+
+	ModeManager:Push(battle);
+end
+
+
+
+function BootMenuTest()
+	print("BootMenuTest");
+end
+
+
+
+function BootShopTest()
+	print("BootShopTest");
+
+	GlobalManager:AddCharacter(1);
+	GlobalManager:AddCharacter(2);
+	GlobalManager:AddCharacter(4);
+	GlobalManager:AddCharacter(8);
+	GlobalManager:AddDrunes(1842);
+	GlobalManager:AddToInventory(1, 5);
+	GlobalManager:AddToInventory(30501, 2);
+	GlobalManager:AddToInventory(2, 3);
+	GlobalManager:AddToInventory(3, 1);
+	GlobalManager:AddToInventory(3002, 1);
+	GlobalManager:AddToInventory(10001, 1);
+	GlobalManager:AddToInventory(10502, 1);
+
+	local shop = hoa_shop.ShopMode();
+	shop:AddObject(1, 3);
+	shop:AddObject(2, 5);
+	shop:AddObject(10501, 2);
+	shop:AddObject(10504, 4);
+	shop:AddObject(3, 12);
+	shop:AddObject(3001, 1);
+	shop:AddObject(30001, 2);
+	shop:AddObject(30002, 3);
+	shop:AddObject(20001, 10);
+	shop:AddObject(20002, 11);
+	shop:AddObject(20501, 2);
+	shop:AddObject(20502, 1);
+
+	ModeManager:Push(shop);
+end
