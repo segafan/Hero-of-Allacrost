e01e5e406afa3920909b9987c318cdea271703c1
rootslinux <rootslinux@localhost>
2011-05-08 22:49:54 +0000

    Cleaned up some editor code

--- game/src/editor/grid.h
+++ game/src/editor/grid.h
@@ -7,12 +7,11 @@
 // See http://www.gnu.org/copyleft/gpl.html for details.
 ///////////////////////////////////////////////////////////////////////////////
 
-/*!****************************************************************************
- * \file    grid.h
- * \author  Philip Vorsilak, gorzuate@allacrost.org
- * \brief   Header file for editor's grid, used for the OpenGL map portion
- *          where tiles are painted, edited, etc.
- *****************************************************************************/
+/** ****************************************************************************
+*** \file    grid.h
+*** \author  Philip Vorsilak, gorzuate@allacrost.org
+*** \brief   Header file for the editor grid,
+*** ***************************************************************************/
 
 #ifndef __GRID_HEADER__
 #define __GRID_HEADER__
@@ -24,167 +23,223 @@
 #include "tileset.h"
 #include "sprites.h"
 
-//! All calls to the editor are wrapped in this namespace.
-namespace hoa_editor
-{
+namespace hoa_editor {
 
-//! Different tile layers in the map.
+//! \brief Different tile layers in the map.
 enum LAYER_TYPE
 {
 	INVALID_LAYER = -1,
-	LOWER_LAYER   = 0,
-	MIDDLE_LAYER  = 1,
-	UPPER_LAYER   = 2,
-	SELECT_LAYER  = 3,
-	OBJECT_LAYER  = 4,
-	TOTAL_LAYER   = 5
+	LOWER_LAYER   =  0,
+	MIDDLE_LAYER  =  1,
+	UPPER_LAYER   =  2,
+	SELECT_LAYER  =  3,
+	OBJECT_LAYER  =  4,
+	TOTAL_LAYER   =  5
 };
 
-//! objects in the object layer.
+//! \brief All types of objects that may be found in the object layer.
 enum OBJECT_TYPE
 {
-	INVALID_OBJECT = -1,
-	VIRTUAL_SPRITE_OBJECT = 0,
-	SPRITE_OBJECT = 1,
-	TOTAL_OBJECT = 2
+	INVALID_OBJECT         = -1,
+	VIRTUAL_SPRITE_OBJECT  =  0,
+	SPRITE_OBJECT          =  1,
+	TOTAL_OBJECT           =  2
 };
+
 
 LAYER_TYPE& operator++(LAYER_TYPE& value, int dummy);
 
 class EditorScrollView;
 
-class Grid: public QGLWidget
-{
-	Q_OBJECT     // macro needed to use QT's slots and signals
+/** ****************************************************************************
+*** \brief Used for the OpenGL map portion where tiles are painted, edited, etc.
+***
+*** This class utilizes the video engine from the game to draw all of the tiles
+*** and objects to the editor's main window screen.
+*** ***************************************************************************/
+class Grid : public QGLWidget {
+	Q_OBJECT // macro needed to use QT's slots and signals
 
-	public:
-		Grid(QWidget *parent = 0, const QString &name = QString("Untitled"),
-			int width = 0, int height = 0); // constructor
-		~Grid();                            // destructor
+public:
+	Grid(QWidget *parent = 0, const QString &name = QString("Untitled"), uint32 width = 0, uint32 height = 0);
 
-		//! Returns the map's modified status.
-		bool GetChanged() const { return _changed; }
-		//! Sets the map's modified status.
-		void SetChanged(bool value);
-		QString GetFileName() const { return _file_name; }
-		void    SetFileName(QString filename); // sets the map's file name
-		int  GetHeight()  const { return _height; }
-		int  GetWidth()   const { return _width; }
-		int  GetContext() const { return _context; }
-		void SetHeight(int height);         // sets the map's height in tiles (rows)
-		void SetWidth(int width);           // sets the map's width in tiles (columns)
-		void SetContext(int context);       // sets the map's active context
-		void SetInitialized(bool ready);    // sets the map's ready to be drawn, completely loaded flag
-		void SetLLOn(bool value);           // sets lower layer on/off
-		void SetMLOn(bool value);           // sets middle layer on/off
-		void SetULOn(bool value);           // sets upper layer on/off
-		void SetOLOn(bool value);			// sets object layer on/off
-		void SetGridOn(bool value);         // sets grid on/off
-		void SetSelectOn(bool value);       // sets selection layer on/off
-		void SetTexturesOn(bool value);     // sets textures on/off
+	~Grid();
 
-		std::vector<int32>& GetLayer(LAYER_TYPE layer, int context);
+	//! \name Class member accessor functions
+	//@{
+	QString GetFileName() const
+		{ return _file_name; }
 
-		/*!
-		 *  \brief Creates a new context for each layer.
-		 *  \param inherit_context - the index of the context to inherit from.
-		 */
-		void CreateNewContext(int inherit_context);
+	int GetHeight() const
+		{ return _height; }
 
-		/*!
-		 *  \brief Loads a map from a config (lua) file when the user selects Open Map... from the File menu.
-		 */
-		void LoadMap();
+	int GetWidth() const
+		{ return _width; }
 
-		/*!
-		 *  \brief Saves the map to a config (lua) file when the user selects Save, Save as..., or Quit
-		 *         from the File menu.
-		 */
-		void SaveMap();
+	int GetContext() const
+		{ return _context; }
 
-		//! \name Context (Right-Clicking) Modification Functions
-		//! \brief Functions to insert or delete rows or columns from the map.
-		//! \note Currently accessed by right-clicking on the map. Could be used elsewhere if the proper
-		//!       tile index is passed as a parameter.
-		//! \param tile_index An ID (0 - width*height-1) of the tile used to determine the row or column
-		//!                   upon which to perform the operation.
-		//{@
-		void InsertRow(int tile_index);
-		void InsertCol(int tile_index);
-		void DeleteRow(int tile_index);
-		void DeleteCol(int tile_index);
-		//@}
+	bool GetChanged() const
+		{ return _changed; }
 
-		//! List of the tileset names being used.
-		QStringList tileset_names;
-		//! A vector which contains a pointer to each tileset and the tiles it has loaded via LoadMultiImage.
-		std::vector<Tileset*> tilesets;
-		//! A list which contains a pointer to each sprite, I use list which is because of its efficient.
-		std::list<MapSprite*> sprites;
+	std::vector<int32>& GetLayer(LAYER_TYPE layer, int context);
 
-		//! A list containing the names of each context.
-		//! \note Should have a max size of 32. That's the max amount of contexts.
-		QStringList context_names;
-		//! A list storing the background music filenames.
-		QStringList music_files;
-	
-		//! pointer to scrollview
-		EditorScrollView * _ed_scrollview;
+	void SetFileName(QString filename)
+		{ _file_name = filename; }
 
-	protected:
-		//! \brief Sets up the rendering context of the OpenGL portion of the editor.
-		void initializeGL();
+	void SetHeight(int height)
+		{ _height = height; _changed = true; }
 
-		//! \brief Paints the entire map with the Allacrost video engine.
-		void paintGL();
+	void SetWidth(int width)
+		{ _width = width; _changed = true; }
 
-		//! \brief Performs a resize operation of the OpenGL widget when appropriate.
-		void resizeGL(int w, int h);
+	void SetContext(int context)
+		{ _context = context; }
 
-	private:
-		//! The map's file name.
-		QString _file_name;
-		//! The height of the map in tiles.
-		int _height;
-		//! The width of the map in tiles.
-		int _width;
-		//! The active context for editing tiles.
-		int _context;
+	void SetChanged(bool value)
+		{ _changed = value; }
 
-		//! When TRUE the map has been modified.
-		bool _changed;
-		//! When TRUE the map is ready to be drawn.
-		bool _initialized;
-		//! When TRUE the grid between tiles is displayed.
-		bool _grid_on;
-		//! When TRUE the rectangle of chosen tiles is displayed.
-		bool _select_on;
-		//! When TRUE the texture sheets are displayed.
-		bool _textures_on;
-		//! When TRUE the lower layer of tiles is displayed.
-		bool _ll_on;
-		//! When TRUE the middle layer of tiles is displayed.
-		bool _ml_on;
-		//! When TRUE the upper layer of tiles is displayed.
-		bool _ul_on;
-		//! When TRUE the object layer of tiles is displayed.
-		bool _ol_on;
+	void SetInitialized(bool ready)
+		{ _initialized = ready; }
 
-		//! A vector of tiles in the lower layer.
-		std::vector<std::vector<int32> > _lower_layer;
-		//! A vector of tiles in the middle layer.
-		std::vector<std::vector<int32> > _middle_layer;
-		//! A vector of tiles in the upper layer.
-		std::vector<std::vector<int32> > _upper_layer;
-		//! A vector of sprites in the object layer.
-		std::vector<int32 > _object_layer;
-		//! A vector of tiles in the selection rectangle. Exists only in the editor,
-		//! not the game. Acts similarly to an actual layer as far as drawing
-		//! is concerned.
-		std::vector<int32> _select_layer;
-}; // class Grid
+	void SetGridOn(bool value)
+		{ _grid_on = value; updateGL(); }
+
+	void SetSelectOn(bool value)
+		{ _select_on = value; updateGL(); }
+
+	void SetTexturesOn(bool value)
+		{ _textures_on = value; updateGL(); }
+
+	void SetLLOn(bool value)
+		{ _ll_on = value; updateGL(); }
+
+	void SetMLOn(bool value)
+		{ _ml_on = value; updateGL(); }
+
+	void SetULOn(bool value)
+		{ _ul_on = value; updateGL(); }
+
+	void SetOLOn(bool value)
+		{ _ol_on = value; updateGL(); }
+	//@}
+
+	/** \brief Creates a new context for each layer.
+	*** \param inherit_context The index of the context to inherit from.
+	**/
+	void CreateNewContext(int inherit_context);
+
+	//! \brief Loads a map from a Lua file when the user selects "Open Map" from the "File" menu.
+	void LoadMap();
+
+	//! \brief Saves the map to a Lua file when the user selects "Save", "Save as", or "Quit" from the "File" menu.
+	void SaveMap();
+
+	/** \name Context Modification Functions (Right-Click)
+	*** \brief Functions to insert or delete rows or columns of tiles from the map.
+	*** \param tile_index An ID (range: {0, width * height - 1}) of the tile used to determine the row or column
+	***        upon which to perform the operation.
+	***
+	*** \note This feature is accessed by right-clicking on the map. It could be used elsewhere if the proper
+	*** tile index is passed as a parameter.
+	**/
+	//{@
+	void InsertRow(int tile_index);
+	void InsertCol(int tile_index);
+	void DeleteRow(int tile_index);
+	void DeleteCol(int tile_index);
+	//@}
+
+	//! \brief List of the tileset names being used.
+	QStringList tileset_names;
+
+	//! \brief A vector which contains a pointer to each tileset and the tiles it has loaded via LoadMultiImage.
+	std::vector<Tileset*> tilesets;
+
+	//! \brief A list which contains a pointer to each sprite, I use list which is because of its efficient.
+	std::list<MapSprite*> sprites;
+
+	/** \brief The names of each individual context
+	*** \note Maximum size is 32 entries, the maximum amount of contexts that a single map supports.
+	**/
+	QStringList context_names;
+
+	//! \brief A list storing the background music filenames.
+	QStringList music_files;
+
+	//! \brief pointer to scrollview
+	EditorScrollView * _ed_scrollview;
+
+protected:
+	//! \brief Sets up the rendering context of the OpenGL portion of the editor.
+	void initializeGL();
+
+	//! \brief Paints the entire map with the Allacrost video engine.
+	void paintGL();
+
+	//! \brief Performs a resize operation of the OpenGL widget when appropriate.
+	void resizeGL(int w, int h);
+
+private:
+	//! \brief The map's file name.
+	QString _file_name;
+
+	//! \brief The height of the map in tiles.
+	uint32 _height;
+
+	//! \brief The width of the map in tiles.
+	uint32 _width;
+
+	//! \brief The active context for editing tiles.
+	uint32 _context;
+
+	//! \brief When TRUE the map has been modified.
+	bool _changed;
+
+	//! \brief When TRUE the map is ready to be drawn.
+	bool _initialized;
+
+	//! \brief When TRUE the grid between tiles is displayed.
+	bool _grid_on;
+
+	//! \brief When TRUE the rectangle of chosen tiles is displayed.
+	bool _select_on;
+
+	//! \brief When TRUE the texture sheets are displayed.
+	bool _textures_on;
+
+	//! \brief When TRUE the lower layer of tiles is displayed.
+	bool _ll_on;
+
+	//! \brief When TRUE the middle layer of tiles is displayed.
+	bool _ml_on;
+
+	//! \brief When TRUE the upper layer of tiles is displayed.
+	bool _ul_on;
+
+	//! \brief When TRUE the object layer of tiles is displayed.
+	bool _ol_on;
+
+	//! \brief A vector of tiles in the lower layer.
+	std::vector<std::vector<int32> > _lower_layer;
+
+	//! \brief A vector of tiles in the middle layer.
+	std::vector<std::vector<int32> > _middle_layer;
+
+	//! \brief A vector of tiles in the upper layer.
+	std::vector<std::vector<int32> > _upper_layer;
+
+	//! \brief A vector of sprites in the object layer.
+	std::vector<int32 > _object_layer;
+
+	/** \brief A vector of tiles in the selection rectangle.
+	***
+	*** This data exists only in the editor and is not a part of the map file nor the game.
+	*** It acts similar to an actual tile layer as far as drawing is concerned.
+	**/
+	std::vector<int32> _select_layer;
+}; // class Grid : public QGLWidget
 
 } // namespace hoa_editor
 
-#endif
-// __GRID_HEADER__
+#endif // __GRID_HEADER__
