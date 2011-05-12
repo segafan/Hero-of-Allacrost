e54c05e92da6d2c3fac1b6263c8ac61b72ca8e56
rootslinux <rootslinux@localhost>
2011-05-12 16:00:48 +0000

    Fixed seg fault bug occuring on cave map by previous commit

--- game/dat/maps/river_access_cave.lua
+++ game/dat/maps/river_access_cave.lua
@@ -25,7 +25,7 @@ sound_filenames = {}
 
 -- The music files used as background music on this map.
 music_filenames = {}
-music_filenames[1] = "mus/mus/Cave2.ogg"
+music_filenames[1] = "mus/Cave2.ogg"
 
 -- The names of the contexts used to improve Editor user-friendliness
 context_names = {}
