b7e6ddf62a3f3d7dcbc2ecbb82197ed974cecf96
rootslinux <rootslinux@localhost>
2015-01-24 03:12:00 +0000

    Reorganizing of interior house tileset and some tileset definition files

--- game/lua/data/tilesets/mountain_landscape.lua
+++ game/lua/data/tilesets/mountain_landscape.lua
@@ -1,18 +1,18 @@
-local ns = {}
-setmetatable(ns, {__index = _G})
-mountain_landscape = ns
-setfenv(1, ns)
+local ns = {};
+setmetatable(ns, {__index = _G});
+mountain_landscape = ns;
+setfenv(1, ns);
 
 tileset_name = "Mountain - Landscape"
 image = "img/tilesets/mountain_landscape.png"
 
 collisions = {}
 collisions[0] = { 0, 1, 3, 3, 3, 0, 0, 1, 3, 0, 0, 0, 0, 0, 0, 0 }
-collisions[1] = { 1, 14, 0, 0, 4, 5, 7, 12, 4, 15, 0, 0, 0, 0, 0, 0 }
-collisions[2] = { 5, 0, 0, 0, 0, 15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
-collisions[3] = { 5, 0, 0, 0, 0, 15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
-collisions[4] = { 4, 3, 0, 0, 7, 15, 11, 3, 3, 3, 0, 0, 0, 0, 0, 0 }
-collisions[5] = { 0, 15, 15, 15, 15, 5, 0, 15, 15, 15, 15, 0, 0, 0, 0, 0 }
+collisions[1] = { 1, 14, 0, 0, 5, 2, 7, 12, 4, 15, 0, 0, 0, 0, 0, 0 }
+collisions[2] = { 5, 0, 0, 0, 0, 10, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0 }
+collisions[3] = { 5, 0, 0, 0, 0, 10, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0 }
+collisions[4] = { 5, 0, 0, 0, 5, 10, 11, 3, 3, 3, 0, 0, 0, 0, 0, 0 }
+collisions[5] = { 5, 15, 15, 15, 15, 8, 13, 15, 15, 15, 0, 0, 0, 0, 0, 0 }
 collisions[6] = { 4, 15, 15, 15, 15, 0, 4, 15, 15, 0, 0, 0, 0, 0, 0, 0 }
 collisions[7] = { 14, 0, 0, 13, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
 collisions[8] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
