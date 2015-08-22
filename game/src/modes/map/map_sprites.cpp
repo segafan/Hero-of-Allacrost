2e196ddaefc9cd24254ca5637a652612d15bf0a0
Atypikal_Arkitect <jibrankhan00@hotmail.com>
2015-08-22 18:09:49 +0300

    Indentation fixes due to tab space setting differences in linux and windows.

--- game/src/modes/map/map_sprites.cpp
+++ game/src/modes/map/map_sprites.cpp
@@ -310,7 +310,7 @@ void VirtualSprite::_ResolveCollision(COLLISION_TYPE coll_type, MapObject* coll_
 			if (enemy_battle_script != "")
 				BM->LoadBattleScript(enemy_battle_script);
 
-            MapMode::CurrentInstance()->_TransitionToMode(BM);
+                        MapMode::CurrentInstance()->_TransitionToMode(BM);
 
 			// TODO: some sort of map-to-battle transition animation sequence needs to start here
 			return;
