98e03eab1100dee9c302a9971ab257bcb78a8ff7
nemesisdd <nemesisdd@localhost>
2015-09-15 17:00:03 +0200

    Reset of the coordinate system when popState() of the video engine. The missing reset was responible for the dialog icon not being drawn. Note, the line endings of map.cpp were mixed between Unix and Windows. Now they are all Unix. Therefore, some line changes are given that are actually no changes.

--- game/src/engine/video/video.cpp
+++ game/src/engine/video/video.cpp
@@ -656,6 +656,12 @@ void VideoEngine::PopState() {
 
 	_current_context = _context_stack.top();
 	_context_stack.pop();
+    
+	// Restore the coordinate_system, i.e. the GL_PROJECTION matrix
+	// NOTE: This will implicitely lead to _current_context.coordinate_system = _current_context.coordinate_system
+	// NOTE: Another option is to also push and pop the matrix of GL_PROJECTION. However, the maximum size of the
+	//       corresponding stack is only ensured to be 2. Therefore, for portrability the is not performed here.
+	SetCoordSys(_current_context.coordinate_system);
 
 	// Restore the modelview transformation
 	glMatrixMode(GL_MODELVIEW);
